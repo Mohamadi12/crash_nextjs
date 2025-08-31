16025f147cd81cdbdfefd1311c5db20c216e6fa0
Nana Mohamadi <adilnana78@gmail.com>
2025-08-31 21:33:49 +0000

    Métadonnées de routage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,6 @@
+export const metadata = {
+  title: 'About Nana Portfolio'
+}
 
 const About = () => {
   return (
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,50 @@
+import { Metadata } from "next";
 
-const ProductDetails = async ({params}:{params: Promise<{productId: string}>}) => {
-    const productId = (await params).productId;
-  return (
-    <div>Product Details { productId }</div>
-  )
-}
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const id = (await params).productId;
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Product ${id}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
 
-export default ProductDetails
+/*
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  const id = params.productId;
+  const res = await fetch(`https://api.example.com/products/${id}`, {
+    next: { revalidate: 60 },
+  });
+  if (!res.ok) {
+    return {
+      title: "Produit introuvable",
+    };
+  }
+  const product = await res.json();
+  return {
+    title: product.name ? `Produit ${product.name}` : `Produit ${id}`,
+    description: product.description,
+    openGraph: {
+      title: product.name,
+      description: product.description,
+      images: [product.imageUrl],
+    },
+  };
+};
+
+*/
+
+const ProductDetails = async ({ params }: Props) => {
+  const productId = (await params).productId;
+  return <div>Product Details {productId}</div>;
+};
+
+export default ProductDetails;

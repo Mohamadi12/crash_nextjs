13c015ba02993ad601b0ffb3acde230265864827
Nana Mohamadi <adilnana78@gmail.com>
2025-08-31 15:57:15 +0000

    Dispositions imbriquées

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,10 @@
+const ProductDetailsLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      {children}
+      <h1>Product Details</h1>
+    </div>
+  );
+};
+
+export default ProductDetailsLayout;

a7127661d949ec9e30f5eb53b180eabe303ddfba
Nana Mohamadi <adilnana78@gmail.com>
2025-09-01 23:38:17 +0000

    Navigation par programmation

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,23 @@
+import { redirect } from "next/navigation";
 
-const ReviewId = async ({params}:{params: Promise<{reviewId: string; productId: string}>}) => {
-    const {reviewId, productId} = await params
+const ReviewId = async ({
+  params,
+}: {
+  params: Promise<{ reviewId: string; productId: string }>;
+}) => {
+  const { reviewId, productId } = await params;
+
+  if (parseInt(reviewId) > 100) {
+    redirect("/products");
+  }
+
   return (
     <div>
-        <h1>Reviews: {reviewId} and for Products: {productId}</h1>
+      <h1>
+        Reviews: {reviewId} and for Products: {productId}
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default ReviewId
+export default ReviewId;

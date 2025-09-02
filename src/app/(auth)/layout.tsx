f7eb559648166edeab001b1a8f2bf2ca9f271b69
Nana Mohamadi <adilnana78@gmail.com>
2025-09-02 10:00:30 +0000

    Modèles

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const navLinks = [
   { name: "Register", href: "/register" },
@@ -10,9 +11,16 @@ const navLinks = [
 ];
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const [input, setInput] = useState("")
   const pathname = usePathname();
+
   return (
     <div>
+      
+      <div className="m-4 bg-amber-100 w-48 text-black">
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+      </div>
+
       {navLinks.map((link) => {
         const isActive =
           pathname === link.href ||

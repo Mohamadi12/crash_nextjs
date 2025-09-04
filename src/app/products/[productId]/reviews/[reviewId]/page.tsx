531292cd3525a0f2f0e3273e0204ec0009f590e5
Nana Mohamadi <adilnana78@gmail.com>
2025-09-04 12:50:46 +0000

    Gestion des erreurs

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,12 +1,20 @@
 import { redirect } from "next/navigation";
 
-const ReviewId = async ({
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
+export default async function ProductReview({
   params,
 }: {
   params: Promise<{ reviewId: string; productId: string }>;
-}) => {
-  const { reviewId, productId } = await params;
+}) {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
 
+  const { reviewId, productId } = await params;
   if (parseInt(reviewId) > 100) {
     redirect("/products");
   }
@@ -18,6 +26,4 @@ const ReviewId = async ({
       </h1>
     </div>
   );
-};
-
-export default ReviewId;
+}

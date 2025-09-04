167239e9b14a50c672d29156aa2c77033dab7eb3
Nana Mohamadi <adilnana78@gmail.com>
2025-09-04 13:32:52 +0000

    Gestion des erreurs dans les mises en page

--- src/app/products/[productId]/layout.tsx
+++ src/app/products/[productId]/layout.tsx
@@ -1,10 +1,20 @@
-const ProductDetailsLayout = ({ children }: { children: React.ReactNode }) => {
+// function getRandomInt(count: number) {
+//   return Math.floor(Math.random() * count);
+// }
+export default function ProductLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // const random = getRandomInt(2);
+  // if (random === 1) {
+  //   throw new Error("Error loading product");
+  // }
+
   return (
     <div>
       {children}
       <h1>Product Details</h1>
     </div>
   );
-};
-
-export default ProductDetailsLayout;
+}

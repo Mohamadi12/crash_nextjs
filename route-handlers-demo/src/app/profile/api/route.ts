7c050ccfc21152818bd1c63a2565821266c3b6cc
Nana Mohamadi <adilnana78@gmail.com>
2025-09-06 05:37:43 +0000

    Cookies dans les gestionnaires de route

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -7,5 +7,18 @@ export async function GET(request: NextRequest) {
 
   const headerList = await headers()
   console.log(headerList.get("Authorization"))
-  return new Response("Profile API data");
+
+  const theme = request.cookies.get("theme")
+  console.log(theme)
+
+  const cookieStore = await cookies()
+  cookieStore.set("resultPerPage", "20")
+  console.log(cookieStore.get("resultPerPage"))
+
+  return new Response("<h1>Profile API data</h1>", { 
+    headers: {
+      "content-type": "text/html",
+      "set-Cookies": "theme=dark"
+    }
+   });
 }

90b8ec1db77537624c6af915b0f1102fbe197807
Nana Mohamadi <adilnana78@gmail.com>
2025-09-06 05:30:28 +0000

    En-têtes dans les gestionnaires de route

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,11 @@
-export async function GET() {
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers)
+  // console.log(requestHeaders.get("Authorization"))
+
+  const headerList = await headers()
+  console.log(headerList.get("Authorization"))
   return new Response("Profile API data");
 }

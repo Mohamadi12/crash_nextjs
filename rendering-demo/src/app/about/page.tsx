23688b1e045c6ca75b2c00d5e977a3a30eade6a8
Nana Mohamadi <adilnana78@gmail.com>
2025-09-08 12:42:34 +0000

    Rendu dynamique(npm run build) pour comprendre

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,5 +1,10 @@
+import { cookies } from "next/headers"
 
-const AboutPage = () => {
+const AboutPage = async() => {
+  const cookieStore = await cookies()
+  const theme = cookieStore.get('theme')
+  console.log('theme', theme)
+  
   return (
     <div>About page {new Date().toLocaleDateString()}</div>
   )

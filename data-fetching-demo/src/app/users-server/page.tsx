2011a17e5bc90cea11add32066c535e097aa12ec
Nana Mohamadi <adilnana78@gmail.com>
2025-09-17 12:44:53 +0000

    États de chargement et d'erreur

--- data-fetching-demo/src/app/users-server/page.tsx
+++ data-fetching-demo/src/app/users-server/page.tsx
@@ -7,7 +7,7 @@ type User = {
 };
 
 export default async function UsersServer() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 3000));
 
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await response.json();
